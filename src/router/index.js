4ceaf1e89b74bff884423ebce3ec920ef5e9ef74
Vw1n <2374150718@qq.com>
2025-07-14 10:46:18 +0800

    Update Router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 // router/index.js
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Login from "../components/Login.vue"
 import Main from "../components/Main.vue"
 
@@ -18,7 +18,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(), // 修改为浏览器历史模式
+  history: createWebHashHistory(), // 修改为浏览器历史模式
   routes
 })
 
